762659708b2a05511fd66b526384a563710bd356
ennuientropy <ennuientropy@gmail.com>
2025-08-21 19:28:30 +0000

    Exercise 1.5 completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,20 +1,21 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = ({title}) => <h1>{title}</h1>
   const Content = ({ parts }) => {
@@ -27,9 +28,9 @@ const App = () => {
   
   return (
     <div>
-      <Header title={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
