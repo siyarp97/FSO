cd57a656549b5a75fa50bed905c9f6ff6359738d
ennuientropy <ennuientropy@gmail.com>
2025-08-21 19:14:19 +0000

    Exercise 1.3 Completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,18 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = ({title}) => <h1>{title}</h1>
   const Total = ({total}) => <p>Number of exercises {total}</p>
@@ -13,9 +20,9 @@ const App = () => {
   const Content = () => {
     return(
       <>
-        <Part part={part1} exercise={exercises1} />
-        <Part part={part2} exercise={exercises2} />
-        <Part part={part3} exercise={exercises3} />
+        <Part part={part1.name} exercise={part1.exercises} />
+        <Part part={part2.name} exercise={part2.exercises} />
+        <Part part={part3.name} exercise={part3.exercises} />
       </>
     )
   }
@@ -23,7 +30,7 @@ const App = () => {
     <div>
       <Header title={course} />
       <Content />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
