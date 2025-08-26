2455b311a544cceef6256e377eb6ab294b43f362
siyarp97 <siyarp97@gmail.com>
2025-08-26 20:12:38 +0300

    part2 exercises 2.1-2.5 completed

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,15 +1,16 @@
-const Header = ({ name }) => <h1>{name}</h1>
-const Content = ({ parts }) => <ul>{
-    parts.map(part => <Part key={part.id} part={part} />)
-}</ul>
-const Part = ({ part }) => <li>{part.name} {part.exercises}</li>
+const Header = ({name}) => <h1>{name}</h1>
+const Content = ({parts}) =>
+    <ul>
+        {parts.map(part => <Part key={part.id} part={part}/>)}
+        <li style={{fontWeight:'bold'}}>Total of {parts.reduce((acc, curr) => acc + curr.exercises,0)} exercises</li>
+    </ul>
+const Part = ({part}) => <li>{part.name} {part.exercises}</li>
 
-
-export const Course = ({ course }) => {
+export const Course = ({course}) => {
     return (
         <>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
+            <Header name={course.name}/>
+            <Content parts={course.parts}/>
         </>
     )
 }
