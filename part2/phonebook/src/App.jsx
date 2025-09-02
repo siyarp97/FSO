0195cfe0c80c3e63679a5c2ef393954790ce65ff
siyarp97 <siyarp97@gmail.com>
2025-09-02 23:42:45 +0300

    Server side feature added to phone book axios and useEffect hook used, part2 exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,20 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {PersonInfo} from "./components/PersonInfo.jsx";
 import {FormComponent} from "./components/FormComponent.jsx";
 import {FilterComponent} from "./components/FilterComponent.jsx";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [number, setNumber] = useState('')
     const [filterPerson, setFilterPerson] = useState('')
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/persons\n")
+            .then(res => setPersons(res.data))
+    }, []);
 
     function AddNewPerson(e) {
         e.preventDefault()
